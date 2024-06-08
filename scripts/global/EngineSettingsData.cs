bd1203fdf60314fe75130a031ce096ba0f629be1
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2024-06-09 00:45:32 +0300

    Cleaned up the engine settings file

--- scripts/global/EngineSettingsData.cs
+++ scripts/global/EngineSettingsData.cs
@@ -1,12 +1,19 @@
-﻿namespace Project.Settings;
+﻿// ReSharper disable UnusedMember.Global, MemberCanBeMadeStatic.Global, FieldCanBeMadeReadOnly.Global
+#pragma warning disable CA1822
+namespace Project.Settings;
 using Godot;
 
 // TODO: Add graphical and brightness stuff
 
 public class Display {
+	// Backing fields
+	private int framerateCap = 120;
+	
+	// Resolution; Defaults to the screen resolution
 	public Vector2I Resolution => DisplayServer.ScreenGetSize();
 
-	[Export(PropertyHint.Range, hintString:"30,250,10")]
+	// Framerate limiter, for battery-saving reasons
+	[Export(PropertyHint.Range, hintString: "30,250,10")]
 	public int FramerateCap {
 		get => framerateCap;
 		set {
@@ -14,36 +21,44 @@ public class Display {
 			Engine.SetMaxFps(framerateCap);
 		}
 	}
-	private int framerateCap = 120;
-	
+
+	/// Nuh uhh
 	public DisplayServer.VSyncMode VSync => DisplayServer.WindowGetVsyncMode();
 }
 
-public class Graphics {}
+public class Graphics {
+	// TODO: Add graphical settings
+}
 
 public class Performance {
-	// TODO: Add several performance modes
+	// Backing fields
+	private bool lowPerfMode = false;
+	private bool updateTitle = true;
+
+	/// Property for LowPerformanceMode
 	public bool LowPerformanceMode {
 		get => lowPerfMode || Utils.IsHomebrew();
 		set => lowPerfMode = value;
 	}
-	private bool lowPerfMode = false;
 
+	/// Property for UpdateTitle; Has a default window title
 	public bool UpdateTitle {
 		get => updateTitle;
 		set {
 			updateTitle = value;
 			var window = (Engine.GetMainLoop() as SceneTree)?.Root;
-			if (!updateTitle && window != null) window.SetTitle("Animatronic Runtime"); 
+			if (!updateTitle && window != null) {
+				window.SetTitle("Animatronic Runtime");
+			}
 		}
 	}
-	private bool updateTitle = true;
 }
 
-/// <para>This stores all the editor settings. </para>
-/// <para>This class procedurally generates UI using <see cref="SettingsMenuParser.Parser.ParseFieldAttributes"/></para>
+/// <summary>
+/// This class stores all the engine settings.
+/// </summary>
 public class EngineSettingsData {
-	public Display Display = new();
-	public Graphics Graphics = new();
-	public Performance Performance = new();
+	public Display Display { get; } = new();
+	public Graphics Graphics { get; } = new();
+	public Performance Performance { get; } = new();
 }
